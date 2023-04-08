7138145749483643ce5154cff82130196870d98b
Cassio Mariano <cassiomariano774@gmail.com>
2023-04-07 21:54:18 -0300

    Recipe App

---
+++ app/src/pages/Recipe.jsx
@@ -0,0 +1,103 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import { useParams } from "react-router-dom";
+
+const Recipe = () => {
+	const [details, setDetails] = useState({});
+	const [activeTab, setActiveTab] = useState("instructions");
+	let params = useParams();
+
+	const fetchDetails = async () => {
+		const data = await fetch(
+			`https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${process.env.REACT_APP_API_KEY}`,
+		);
+		const detailData = await data.json();
+		setDetails(detailData);
+	};
+
+	useEffect(() => {
+		
+		fetchDetails();
+		// eslint-disable-next-line
+	}, [params.name]);
+	return (
+		<DetailWrapper>
+			<div>
+				<h2>{details.title}</h2>
+				<img src={details.image} alt={details.title} srcset="" />
+			</div>
+			<Info>
+				<Button
+					className={activeTab === "instructions" ? "active" : ""}
+					onClick={() => setActiveTab("instructions")}
+				>
+					Instructions
+				</Button>
+				<Button
+					className={activeTab === "ingredients" ? "active" : ""}
+					onClick={() => setActiveTab("ingredients")}
+				>
+					Ingredients
+				</Button>
+
+				{activeTab === "instructions" && (
+					<div>
+						<h3 dangerouslySetInnerHTML={{ __html: details.summary }}></h3>
+						<h3 dangerouslySetInnerHTML={{ __html: details.instructions }}></h3>
+					</div>
+				)}
+
+				{activeTab === "ingredients" && (
+					<ul>
+						{details.extendedIngredients.map((ingredient) => {
+							return <li key={ingredient.id}>{ingredient.original}</li>;
+						})}
+					</ul>
+				)}
+			</Info>
+		</DetailWrapper>
+	);
+};
+
+const DetailWrapper = styled.div`
+	margin-top: 10rem;
+	margin-bottom: 5rem;
+	display: flex;
+	div img {
+		max-width: 400px;
+	}
+	.active {
+		background: linear-gradient(35deg, #494949, #313131);
+		color: white;
+	}
+	h2 {
+		margin-bottom: 2rem;
+		max-width: 500px;
+		font-size: 1.3rem;
+		text-overflow: ellipsis;
+	}
+
+	ul {
+		margin-top: 2rem;
+	}
+	h3,
+	li {
+		font-size: 0.9rem;
+	}
+`;
+
+const Button = styled.button`
+	padding: 1rem 2rem;
+	color: #313131;
+	background: white;
+	border: 2px solid black;
+	margin-right: 2rem;
+	font-weight: 600;
+	cursor: pointer;
+`;
+
+const Info = styled.div`
+	margin-left: 10rem;
+`;
+
+export default Recipe;
